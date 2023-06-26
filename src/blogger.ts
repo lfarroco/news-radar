d17e4ef3d39f72161d41012bd3d77fafb35cd37a
Leonardo Farroco <leonardo.farroco@gmail.com>
2023-06-26 06:47:19 -0300

    automated run

--- src/blogger.ts
+++ src/blogger.ts
@@ -1,37 +1,53 @@
 import { google } from 'googleapis';
 import { config } from 'dotenv';
-import { authenticate } from '@google-cloud/local-auth';
-import * as path from 'path';
-import { fileURLToPath } from 'url';
 
 config();
 
 // Each API may support multiple versions. With this sample, we're getting
 // v3 of the blogger API, and using an API key to authenticate.
-const blogger = google.blogger('v3');
-
-const dirname = path.dirname(fileURLToPath(import.meta.url));
-
-const auth = await authenticate({
-  keyfilePath: path.join(dirname, '../../blogger-secret.json'),
-  scopes: 'https://www.googleapis.com/auth/blogger',
+const blogger = google.blogger({
+  version: 'v3',
+  auth: "AIzaSyCrt6W8uR5YKsIn7w5PCb0oPmMaT8dHrtE",
 });
-google.options({ auth });
 
-console.log('Tokens:', auth.credentials);
 
 const publish = async () => {
   const res = await blogger.posts.insert({
     blogId: '2717877600688481984',
     requestBody: {
-      title: 'second post',
+      title: 'Hello from the googleapis npm module!',
       content:
-        'testttt',
+        'Visit https://github.com/google/google-api-nodejs-client to learn more!',
     },
   });
 
   console.log(res.data);
   return res.data;
 };
+// const blogger = google.blogger('v3');
+
+// const dirname = path.dirname(fileURLToPath(import.meta.url));
+
+// const auth = await authenticate({
+//   keyfilePath: path.join(dirname, '../../blogger-secret.json'),
+//   scopes: 'https://www.googleapis.com/auth/blogger',
+// });
+// google.options({ auth });
+
+// console.log('Tokens:', auth.credentials);
+
+// const publish = async () => {
+//   const res = await blogger.posts.insert({
+//     blogId: '2717877600688481984',
+//     requestBody: {
+//       title: 'second post',
+//       content:
+//         'testttt',
+//     },
+//   });
+
+//   console.log(res.data);
+//   return res.data;
+// };
 
 await publish();
