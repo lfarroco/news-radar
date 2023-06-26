140459296e06d01769572360283285506a711b0b
Leonardo Farroco <leonardo.farroco@gmail.com>
2023-06-25 21:12:24 -0300

    automated run

--- src/blogger.ts
+++ src/blogger.ts
@@ -1,23 +1,37 @@
 import { google } from 'googleapis';
 import { config } from 'dotenv';
+import { authenticate } from '@google-cloud/local-auth';
+import * as path from 'path';
+import { fileURLToPath } from 'url';
 
 config();
 
 // Each API may support multiple versions. With this sample, we're getting
 // v3 of the blogger API, and using an API key to authenticate.
-const blogger = google.blogger({
-  version: 'v3',
-  auth: process.env.BLOGGER_KEY,
-});
+const blogger = google.blogger('v3');
 
-const publish = async () =>
-  blogger.posts.insert({
+const dirname = path.dirname(fileURLToPath(import.meta.url));
+
+const auth = await authenticate({
+  keyfilePath: path.join(dirname, '../../blogger-secret.json'),
+  scopes: 'https://www.googleapis.com/auth/blogger',
+});
+google.options({ auth });
+
+console.log('Tokens:', auth.credentials);
+
+const publish = async () => {
+  const res = await blogger.posts.insert({
     blogId: '2717877600688481984',
     requestBody: {
-      title: 'Hello from the googleapis npm module!',
+      title: 'second post',
       content:
-        'Visit https://github.com/google/google-api-nodejs-client to learn more!',
+        'testttt',
     },
   });
+
+  console.log(res.data);
+  return res.data;
+};
 
 await publish();
