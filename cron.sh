02565fd25370c05c9c0a8ea54ff02288bf1a9b5a
Leonardo Farroco <leonardo.farroco@gmail.com>
2023-06-28 12:43:58 -0300

    automated run

--- cron.sh
+++ cron.sh
@@ -1,13 +1,8 @@
 #!/bin/sh  
 while true  
 do  
-  #only run if there's a git change
-  if [ -n "$(git status --porcelain)" ]; then
-    make run
-    echo "Finished running at: $(date). Next check in 1 hour."
-  else
-    echo "No changes in repo at: $(date). Next check in 1 hour."
-  fi
+  make run
+  echo "Finished running at $(date)"
   #every hour 
   sleep 3600
 done
