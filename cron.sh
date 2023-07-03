3e4bf5c080f5fcad368f66f6e40f5dc7f556ef13
Leonardo Farroco <leonardo.farroco@gmail.com>
2023-07-03 09:40:25 -0300

    automated run

--- cron.sh
+++ cron.sh
@@ -1,7 +1,10 @@
 #!/bin/sh  
 while true  
 do  
+  echo "Starting automated run at $(date)"
+  say "Starting automated run at $(date)"
   make run
+  finished "Finished running at $(date)"
   echo "Finished running at $(date)"
   #every hour 
   sleep 3600
