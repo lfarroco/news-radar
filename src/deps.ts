275f0ee337b0fe995c1ce2c65d17126bb28d7a92
Leonardo Farroco <leonardo.farroco@gmail.com>
2023-07-20 11:12:45 -0300

    automated run

--- src/deps.ts
+++ src/deps.ts
@@ -1,5 +1,5 @@
-import { Client } from "https://deno.land/x/postgres@v0.17.0/mod.ts";
-import { parseFeed } from "https://deno.land/x/rss/mod.ts";
+import { Client } from "postgres";
+import { parseFeed } from "rss";
 import * as cheerio from "https://esm.sh/cheerio@1.0.0-rc.12";
 
 export { Client, parseFeed, cheerio }
