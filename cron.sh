3a5014ed822ae7e9e776f4d495ff5c76ab3823d9
Leonardo Farroco <leonardo.farroco@gmail.com>
2023-08-16 09:32:55 -0300

    use single branch

--- cron.sh
+++ cron.sh
@@ -3,25 +3,20 @@ while true
 do  
   echo "Starting automated run at $(date)"
 
-  git checkout publish
-  git merge main
-
-  if !  make run ; then
+  if ! make run ; then
     echo "Failed to run scanner"
     break
   fi
 
-  if !  make build-pages ; then
+  if ! make build-pages ; then
     echo "Failed to build pages"
-    git checkout main
     break
   fi
 
   git add _site/
   git commit -m "automated run at $(date)"
-  git push origin publish
+  git push origin main
   make dump-db
-  git checkout main
 
   echo "Finished running at $(date)"
   #every hour 
